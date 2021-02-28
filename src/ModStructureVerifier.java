4a201107002586d29220e212647bf2324322bd70
max4805 <max480@free.fr>
2021-02-28 16:14:21 +0100

    Added HttpPostMultipart to the repo

--- src/ModStructureVerifier.java
+++ src/ModStructureVerifier.java
@@ -1,6 +1,5 @@
 package com.max480.discord.randombots;
 
-import com.max480.quest.modmanagerbot.HttpPostMultipart;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
