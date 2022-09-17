dd8c51d136022bab06b9e7a9ca9630a99f4531d3
max4805 <max480@free.fr>
2022-09-17 17:36:06 +0200

    Allow replacing or deleting tweets

--- src/celeste-backend-crontabs/OlympusNewsGenerator.java
+++ src/celeste-backend-crontabs/OlympusNewsGenerator.java
@@ -12,7 +12,6 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -24,13 +23,46 @@ public class OlympusNewsGenerator {
     private static final Logger log = LoggerFactory.getLogger(OlympusNewsGenerator.class);
 
     public static void generateFeed(JSONArray tweetFeed) throws IOException {
-        List<JSONObject> output = new ArrayList<>();
+        // download the manual Olympus news entries
+        JSONObject manualNews;
+        try (InputStream is = ConnectionUtils.openStreamWithTimeout(new URL("https://raw.githubusercontent.com/max4805/RandomBackendStuff/main/olympusnews.json"))) {
+            manualNews = new JSONObject(IOUtils.toString(is, StandardCharsets.UTF_8));
+        }
+        final JSONArray manualAddBefore = manualNews.getJSONArray("add_before");
+        final JSONArray manualAddAfter = manualNews.getJSONArray("add_after");
+        final JSONObject manualReplace = manualNews.getJSONObject("replace");
+        final JSONArray manualDelete = manualNews.getJSONArray("delete");
 
-        for (int i = 0; i < tweetFeed.length() && output.size() < 10; i++) {
+        JSONArray output = new JSONArray();
+
+        // add tweets that have to be inserted before
+        for (Object o : manualAddBefore) {
+            output.put(o);
+        }
+
+        for (int i = 0; i < tweetFeed.length() && output.length() < 10 + manualAddBefore.length(); i++) {
             JSONObject tweet = tweetFeed.getJSONObject(i);
+            String tweetId = tweet.getString("id_str");
 
             // do not include RTs
             if (tweet.has("retweeted_status")) {
+                continue;
+            }
+
+            // skip tweets that were marked as "delete" in manual news
+            boolean isDeleted = false;
+            for (int q = 0; q < manualDelete.length(); q++) {
+                if (manualDelete.getString(q).equals(tweetId)) {
+                    manualDelete.remove(q);
+                    isDeleted = true;
+                }
+            }
+            if (isDeleted) continue;
+
+            // replace tweets that were marked as "replace"
+            if (manualReplace.has(tweetId)) {
+                output.put(manualReplace.getJSONObject(tweetId));
+                manualReplace.remove(tweetId);
                 continue;
             }
 
@@ -75,33 +107,16 @@ public class OlympusNewsGenerator {
                 }
             }
 
-            output.add(mappedTweet);
+            output.put(mappedTweet);
         }
 
-        // download the manual Olympus news entries...
-        JSONArray manualNews;
-        try (InputStream is = ConnectionUtils.openStreamWithTimeout(new URL("https://raw.githubusercontent.com/max4805/RandomBackendStuff/main/olympusnews.json"))) {
-            manualNews = new JSONArray(IOUtils.toString(is, StandardCharsets.UTF_8));
-        }
-
-        // ... and insert them in the list
-        int topPosition = 0;
-        for (int i = 0; i < manualNews.length(); i++) {
-            JSONObject manualNewsEntry = manualNews.getJSONObject(i);
-
-            String position = manualNewsEntry.getString("position");
-            manualNewsEntry.remove("position");
-
-            if (position.equals("top")) {
-                output.add(topPosition, manualNewsEntry);
-                topPosition++;
-            } else {
-                output.add(manualNewsEntry);
-            }
+        // add tweets that have to be inserted after
+        for (Object o : manualAddAfter) {
+            output.put(o);
         }
 
         // push to Cloud Storage
-        CloudStorageUtils.sendStringToCloudStorage(new JSONArray(output).toString(), "olympus_news.json", "application/json");
+        CloudStorageUtils.sendStringToCloudStorage(output.toString(), "olympus_news.json", "application/json");
 
         // update the frontend cache
         HttpURLConnection conn = (HttpURLConnection) new URL("https://max480-random-stuff.appspot.com/celeste/olympus-news-reload?key="
@@ -115,7 +130,9 @@ public class OlympusNewsGenerator {
         WebhookExecutor.executeWebhook(SecretConstants.PERSONAL_TWITTER_WEBHOOK_URL,
                 "https://cdn.discordapp.com/attachments/445236692136230943/878508600509726730/unknown.png",
                 "Everest Update Checker",
-                ":sparkles: Olympus news were updated.");
+                ":sparkles: Olympus news were updated.\n" +
+                        "Unused replace entries: [" + String.join(", ", manualReplace.toMap().keySet()) + "]\n" +
+                        "Unused delete entries: " + manualDelete.toString().replace("\"", "").replace(",", ", "));
     }
 
     private static String getLinkTarget(String url) {
