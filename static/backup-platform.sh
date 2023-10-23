d246199c280182a7ee6f307b51dbaa5c77eca914
maddie480 <52103563+maddie480@users.noreply.github.com>
2023-10-23 23:24:59 +0200

    Fix script

--- static/backup-platform.sh
+++ static/backup-platform.sh
@@ -2,5 +2,6 @@
 
 set -xeo pipefail
 
-tar czf /shared/temp/bak_bot_files.tar.gz --exclude="/shared/temp" /tmp/backup-qmm-database /tmp/backup-discord-pins /shared /backend
+rm -f /shared/temp/bak_bot_files.tar.gz
+tar czf /shared/temp/bak_bot_files.tar.gz --exclude="/shared/temp" /tmp/backup-discord-pins /shared /backend
 rm -rf /tmp/backup-discord-pins
