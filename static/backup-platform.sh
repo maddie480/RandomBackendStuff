0a7e951351066a26b60a8036334a2d3465e8b407
maddie480 <52103563+maddie480@users.noreply.github.com>
2023-10-26 21:15:50 +0200

    Rename backup file

--- static/backup-platform.sh
+++ static/backup-platform.sh
@@ -2,6 +2,6 @@
 
 set -xeo pipefail
 
-rm -f /shared/temp/bak_bot_files.tar.gz
-tar czf /shared/temp/bak_bot_files.tar.gz --exclude="/shared/temp" /tmp/backup-discord-pins /shared /backend
+rm -f /shared/temp/RandomStuffBackend.tar.gz
+tar czf /shared/temp/RandomStuffBackend.tar.gz --exclude="/shared/temp" /tmp/backup-discord-pins /shared /backend
 rm -rf /tmp/backup-discord-pins
