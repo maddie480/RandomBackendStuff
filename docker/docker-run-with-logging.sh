3166c868fc9a6a46b65f7e991fd82249892706ea
Maddie <52103563+maddie480@users.noreply.github.com>
2024-09-29 19:52:58 +0200

    Synchronize docker config with server

--- docker/docker-run-with-logging.sh
+++ docker/docker-run-with-logging.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker compose -f /home/debian/$1.yaml run --rm $1 > /home/debian/logs/$(date +%Y%m%d_%H%M%S)_$1.backend.log 2>&1
+
+docker compose -f /home/debian/$1.yaml up > /home/debian/logs/$(date +%Y%m%d_%H%M%S)_$1.backend.log 2>&1
+docker compose -f /home/debian/$1.yaml down
