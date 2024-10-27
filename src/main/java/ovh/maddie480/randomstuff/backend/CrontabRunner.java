4b1850b5a7c6f3d71e963044c89bfb81cd915ede
Maddie <52103563+maddie480@users.noreply.github.com>
2024-10-27 16:32:15 +0100

    Update CrontabRunner.java (time zone offset change)

--- src/main/java/ovh/maddie480/randomstuff/backend/CrontabRunner.java
+++ src/main/java/ovh/maddie480/randomstuff/backend/CrontabRunner.java
@@ -276,7 +276,7 @@ public class CrontabRunner {
             ZonedDateTime runUntil = ZonedDateTime.now(ZoneId.of("UTC")).plusHours(1)
                     .withMinute(0).withSecond(0).withNano(0);
 
-            while (runUntil.getHour() % 6 != 5) {
+            while (runUntil.getHour() % 6 != 0) {
                 runUntil = runUntil.plusHours(1);
             }
 
