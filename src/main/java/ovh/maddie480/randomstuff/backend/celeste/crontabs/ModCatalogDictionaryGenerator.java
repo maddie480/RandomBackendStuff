9137adc5b28c5579ddcfc3e225e59037d8d667b3
Maddie <52103563+maddie480@users.noreply.github.com>
2025-01-03 20:53:19 +0100

    Fix custom entity catalog generation blowing up due to zip file encoding

--- src/main/java/ovh/maddie480/randomstuff/backend/celeste/crontabs/ModCatalogDictionaryGenerator.java
+++ src/main/java/ovh/maddie480/randomstuff/backend/celeste/crontabs/ModCatalogDictionaryGenerator.java
@@ -4,18 +4,22 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ovh.maddie480.everest.updatechecker.YamlUtil;
+import ovh.maddie480.everest.updatechecker.ZipFileWithAutoEncoding;
 import ovh.maddie480.randomstuff.backend.utils.ConnectionUtils;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.zip.ZipInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class ModCatalogDictionaryGenerator {
     private static final Pattern regex = Pattern.compile("^(?:entities|triggers|style\\.effects)\\.([^.]+)\\.(?:placements\\.)?name(?:\\.[^=]+)?=(.*)$", Pattern.MULTILINE);
@@ -39,23 +43,36 @@ public class ModCatalogDictionaryGenerator {
 
         Map<String, Set<String>> dictionary = new TreeMap<>();
 
+        Path tempZip = Paths.get("/tmp/catalogscanner.zip");
+
         for (String url : toCheck) {
             logger.debug("Scanning {}", url);
-            try (ZipInputStream is = new ZipInputStream(ConnectionUtils.openStreamWithTimeout(url))) {
-                while (!is.getNextEntry().getName().equals("Loenn/lang/en_gb.lang")) continue;
+            try (InputStream is = ConnectionUtils.openStreamWithTimeout(url);
+                 OutputStream os = Files.newOutputStream(tempZip)) {
 
-                Matcher matcher = regex.matcher(IOUtils.toString(is, StandardCharsets.UTF_8));
-                while (matcher.find()) {
-                    if (dictionary.containsKey(matcher.group(1))) {
-                        logger.debug("Adding {} => {}", matcher.group(1), matcher.group(2).trim());
-                        dictionary.get(matcher.group(1)).add(matcher.group(2).trim());
-                    } else {
-                        logger.debug("Found {} => {}", matcher.group(1), matcher.group(2).trim());
-                        dictionary.put(matcher.group(1), new LinkedHashSet<>(Collections.singleton(matcher.group(2).trim())));
+                IOUtils.copy(is, os);
+            }
+
+            try (ZipFile file = ZipFileWithAutoEncoding.open(tempZip.toString())) {
+                ZipEntry entry = file.getEntry("Loenn/lang/en_gb.lang");
+                if (entry == null) continue;
+
+                try (InputStream is = file.getInputStream(entry)) {
+                    Matcher matcher = regex.matcher(IOUtils.toString(is, StandardCharsets.UTF_8));
+                    while (matcher.find()) {
+                        if (dictionary.containsKey(matcher.group(1))) {
+                            logger.debug("Adding {} => {}", matcher.group(1), matcher.group(2).trim());
+                            dictionary.get(matcher.group(1)).add(matcher.group(2).trim());
+                        } else {
+                            logger.debug("Found {} => {}", matcher.group(1), matcher.group(2).trim());
+                            dictionary.put(matcher.group(1), new LinkedHashSet<>(Collections.singleton(matcher.group(2).trim())));
+                        }
                     }
                 }
             }
         }
+
+        Files.delete(tempZip);
 
         Map<String, String> result = new TreeMap<>();
         for (Map.Entry<String, Set<String>> entry : dictionary.entrySet()) {
