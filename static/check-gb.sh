45661e2f3473c0151ad7d0c56f28db098b7b1d79
Maddie <52103563+maddie480@users.noreply.github.com>
2025-06-04 23:07:50 +0200

    Fix check_gb.sh
    ... who cares though

--- static/check-gb.sh
+++ static/check-gb.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-GAMEBANANA_CATEGORIES=`curl -s --fail --compressed -H "User-Agent: Maddie-Random-Stuff-Backend/1.0.0 (+https://github.com/maddie480/RandomBackendStuff)" "https://api.gamebanana.com/Core/List/New/AllowedItemTypes?format=yaml" | cut -c 3- | grep -v "-" | grep -v "\\." | sort`
+GAMEBANANA_CATEGORIES=`curl -s --fail --compressed -H "User-Agent: Maddie-Random-Stuff-Backend/1.0.0 (+https://github.com/maddie480/RandomBackendStuff)" "https://api.gamebanana.com/Core/List/New/AllowedItemTypes" | grep -v '\[' | grep -v '\]' | sed "s/[ \",]//g" | sort`
 
 for a in $GAMEBANANA_CATEGORIES
 do
