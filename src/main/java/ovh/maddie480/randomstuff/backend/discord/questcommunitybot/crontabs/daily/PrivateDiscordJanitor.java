2dff46d8a97ce0d24a219956a407b6ec61bc04a8
Maddie <52103563+maddie480@users.noreply.github.com>
2025-08-25 22:08:58 +0200

    Update PrivateDiscordJanitor.java

--- src/main/java/ovh/maddie480/randomstuff/backend/discord/questcommunitybot/crontabs/daily/PrivateDiscordJanitor.java
+++ src/main/java/ovh/maddie480/randomstuff/backend/discord/questcommunitybot/crontabs/daily/PrivateDiscordJanitor.java
@@ -82,9 +82,7 @@ public class PrivateDiscordJanitor {
     }
 
     private boolean shouldPurge(Message message, List<Message> pins, OffsetDateTime delay) {
-        boolean old = message.getTimeCreated().isBefore(delay);
-        boolean spammy = (message.getChannel().getIdLong() == 445631337315958796L && message.getAuthor().getEffectiveName().startsWith("L"));
-        boolean pinned = pins.stream().anyMatch(pin -> pin.getIdLong() == message.getIdLong());
-        return (old || spammy) && !pinned;
+        return message.getTimeCreated().isBefore(delay)
+                && pins.stream().noneMatch(pin -> pin.getIdLong() == message.getIdLong());
     }
 }
