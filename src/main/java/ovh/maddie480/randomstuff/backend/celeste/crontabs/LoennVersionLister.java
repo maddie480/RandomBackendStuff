1e5e46272f62aad667ddf7c759981db42f70f075
Maddie <52103563+maddie480@users.noreply.github.com>
2025-11-11 22:52:10 +0100

    Exclude download_count from Lönn version API

--- src/main/java/ovh/maddie480/randomstuff/backend/celeste/crontabs/LoennVersionLister.java
+++ src/main/java/ovh/maddie480/randomstuff/backend/celeste/crontabs/LoennVersionLister.java
@@ -1,13 +1,13 @@
 package ovh.maddie480.randomstuff.backend.celeste.crontabs;
 
-import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import ovh.maddie480.randomstuff.backend.SecretConstants;
 import ovh.maddie480.randomstuff.backend.utils.ConnectionUtils;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -19,10 +19,20 @@ public class LoennVersionLister {
         HttpURLConnection connAuth = ConnectionUtils.openConnectionWithTimeout("https://api.github.com/repos/CelestialCartographers/Loenn/releases/latest");
         connAuth.setRequestProperty("Authorization", "Basic " + SecretConstants.GITHUB_BASIC_AUTH);
 
-        try (InputStream is = ConnectionUtils.connectionToInputStream(connAuth);
-             OutputStream os = Files.newOutputStream(Paths.get("/shared/celeste/loenn-versions.json"))) {
+        JSONObject releaseInfo;
+        try (InputStream is = ConnectionUtils.connectionToInputStream(connAuth)) {
+            releaseInfo = new JSONObject(new JSONTokener(is));
+        }
 
-            IOUtils.copy(is, os);
+        // download_count isn't used and creates noise (fake changes to the JSON)
+        for (Object asset : releaseInfo.getJSONArray("assets")) {
+            ((JSONObject) asset).remove("download_count");
+        }
+
+        try (OutputStream os = Files.newOutputStream(Paths.get("/shared/celeste/loenn-versions.json"));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os, StandardCharsets.UTF_8))) {
+
+            releaseInfo.write(bw);
         }
     }
 }
