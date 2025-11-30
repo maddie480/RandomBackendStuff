fcccb016f22eb40f0bf73bbd57839b9eb7b7df23
Maddie <52103563+maddie480@users.noreply.github.com>
2025-11-30 22:51:12 +0100

    force TAS check update

--- src/main/java/ovh/maddie480/randomstuff/backend/celeste/crontabs/TASCheckUpdate.java
+++ src/main/java/ovh/maddie480/randomstuff/backend/celeste/crontabs/TASCheckUpdate.java
@@ -163,11 +163,6 @@ fi
     };
 
     public static void main(String[] args) throws Exception {
-        if (!LocalDate.now().equals(EverestPRLabelSlapper.getNextRollingReleaseDate())) {
-            log.info("This isn't rolling release day, skipping");
-            return;
-        }
-
         log.debug("Fetching versions...");
         Map<String, String> fields = new HashMap<>();
         fields.put("{{CelesteTAS-SHA}}", getLatestCommitSHA("VampireFlower/CelesteTAS"));
