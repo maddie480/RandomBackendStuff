b05889d5de7a3b6874dac09c91761c3476dced91
Maddie <52103563+maddie480@users.noreply.github.com>
2025-11-30 20:18:25 +0100

    Update docker-run-hourly-crontabs.sh

--- docker/docker-run-hourly-crontabs.sh
+++ docker/docker-run-hourly-crontabs.sh
@@ -7,7 +7,7 @@ if [ -f backend/daily_lock ]; then
 	sed -i 's,MEMHERE,256M,' hourly-crontabs.yaml
 else
 	# the code mod decompilation check needs more memory, and is disabled during daily crontabs
-	sed -i 's,MEMHERE,512M,' hourly-crontabs.yaml
+	sed -i 's,MEMHERE,576M,' hourly-crontabs.yaml
 fi
 
 ./docker-run-with-logging.sh hourly-crontabs
