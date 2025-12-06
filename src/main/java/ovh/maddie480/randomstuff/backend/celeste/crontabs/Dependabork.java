55f6a1cbf48bd6fbf8aca03d041ba82b033f3f9d
Maddie <52103563+maddie480@users.noreply.github.com>
2025-12-07 00:03:53 +0100

    Dependabork: enroll Collab Utils

--- src/main/java/ovh/maddie480/randomstuff/backend/celeste/crontabs/Dependabork.java
+++ src/main/java/ovh/maddie480/randomstuff/backend/celeste/crontabs/Dependabork.java
@@ -32,6 +32,7 @@ public class Dependabork {
         String status;
         status = process("maddie480", "MaddieHelpingHand", "master");
         status = process("maddie480", "ExtendedVariantMode", "master");
+        status = process("EverestAPI", "CelesteCollabUtils2", "master");
         FileUtils.writeStringToFile(stateFile, status, "UTF-8");
     }
 
