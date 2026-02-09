744ebf756588b55b21e3b32a1d31fab8be9822fc
Maddie <52103563+maddie480@users.noreply.github.com>
2026-02-09 10:01:12 +0100

    Update TASCheckUpdate.java

--- src/main/java/ovh/maddie480/randomstuff/backend/celeste/crontabs/TASCheckUpdate.java
+++ src/main/java/ovh/maddie480/randomstuff/backend/celeste/crontabs/TASCheckUpdate.java
@@ -163,7 +163,7 @@ fi
     };
 
     public static void main(String[] args) throws Exception {
-        if (LocalDate.now().equals(EverestPRLabelSlapper.getNextRollingReleaseDate())) {
+        if (!LocalDate.now().equals(EverestPRLabelSlapper.getNextRollingReleaseDate())) {
             log.info("This isn't rolling release day, skipping");
             return;
         }
